048c6e564a07b94a7fe33db9ccf40dc0e63f4dbc
Alonel7 <alizodanasim0@gmail.com>
2025-12-27 16:55:50 +0500

    add zabones

--- src/pages/registerPage/loginPage/loginPage.tsx
+++ src/pages/registerPage/loginPage/loginPage.tsx
@@ -84,7 +84,7 @@ const LoginPage = () => {
             <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7m0 0l7-7m-7 7h18" />
             </svg>
-            Return to previous page
+            Return to previous page !
           </Link>
         </div>
       </div>
